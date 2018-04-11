3c62d6717a743392ba13b63eadf4ccb4d04806af
hexin <hexin.work@outlook.com>
2018-04-11 10:22:51 +0800

    chapter1 code refine

--- Chapter1/Customer.java
+++ Chapter1/Customer.java
@@ -3,44 +3,43 @@ import java.util.Vector;
 
 class Customer {
 
-	private String _name;
-	private Vector _rentals = new Vector();
+    private String _name;
+    private Vector _rentals = new Vector();
 
-	public Customer (String name){
-		_name = name;
-	};
+    public Customer(String name) {
+        _name = name;
+    }
 
-	public void addRental(Rental arg) {
-		_rentals.addElement(arg);
-	}
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
 
-	public String getName (){
-		return _name;
-	};
+    public String getName() {
+        return _name;
+    }
 
-	public String statement() {
+    public String statement() {
 
-		Enumeration rentals = _rentals.elements();
-		String result = "Rental Record for " + getName() + "\n";
+        Enumeration rentals = _rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
 
-		while (rentals.hasMoreElements()) {
-			Rental each = (Rental) rentals.nextElement();
-
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
             //show figures for this rental
-			result += "\t" + each.getMovie().getTitle()+ "\t" +
-			    String.valueOf(each.getCharge()) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                    String.valueOf(each.getCharge()) + "\n";
 
-		}
-		
-		//add footer lines
-		result += "Amount owed is " + String.valueOf(getTotalCharge()) +"\n";
-		result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+        }
+
+        //add footer lines
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
                 " frequent renter points";
-		return result;
+        return result;
 
-	}
+    }
 
-	public String htmlStatement() {
+    public String htmlStatement() {
         Enumeration rentals = _rentals.elements();
         String result = "<H1>Rental Record for <EM>" + getName() + "</EM></H1><P>\n";
 
@@ -48,37 +47,37 @@ class Customer {
             Rental each = (Rental) rentals.nextElement();
 
             //show figures for this rental
-            result += each.getMovie().getTitle()+ ":" +
+            result += each.getMovie().getTitle() + ":" +
                     String.valueOf(each.getCharge()) + "<BR>\n";
 
         }
 
         //add footer lines
-        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +"</EM><P>\n";
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
         result += "On this rental you earned <EM>" +
                 String.valueOf(getTotalFrequentRenterPoints()) +
                 "<EM> frequent renter points<P>";
         return result;
     }
 
-	private double getTotalCharge() {
-	    double result = 0;
-	    Enumeration rentals = _rentals.elements();
-	    while (rentals.hasMoreElements()) {
-	        Rental each = (Rental) rentals.nextElement();
-	        result += each.getCharge();
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
         }
 
         return result;
     }
 
-    private int getTotalFrequentRenterPoints(){
-	    int result = 0;
-	    Enumeration rentals = _rentals.elements();
-	    while (rentals.hasMoreElements()) {
-	        Rental each = (Rental) rentals.nextElement();
-	        result += each.getFrequentRenterPoints();
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
-	    return result;
+        return result;
     }
 }
--- Chapter1/Movie.java
+++ Chapter1/Movie.java
@@ -54,19 +54,19 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
-    
+
     private String _title;
     private Price _price;
-    
+
     public Movie(String title, int priceCode) {
         _title = title;
         setPriceCode(priceCode);
     }
 
     public int getPriceCode() {
-      return _price.getPriceCode();
+        return _price.getPriceCode();
     }
-    
+
     public void setPriceCode(int arg) {
         switch (arg) {
             case REGULAR:
@@ -82,10 +82,10 @@ public class Movie {
                 throw new IllegalArgumentException("Incorrect Price Code");
         }
     }
-    
-    public String getTitle (){
-      return _title;
-    };
+
+    public String getTitle() {
+        return _title;
+    }
 
     public double getCharge(int daysRented) {
         return _price.getCharge(daysRented);
